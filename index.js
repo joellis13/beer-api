6280efb1dcac79265ccc7b842119572e9e2007c9
Jonathan Ellis <jonathan.ellis@daugherty.com>
2019-02-06 10:26:13 -0600

    created beerSchema

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const app = express();
+const beerRouter = require('./routes/beerRouter');
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+
 /////////////////////////////////////
 
+app.use('/api/beers', beerRouter);
 
+app.use('/verb', (req, res) => {
+    const method = req.method;
+    res.send(method);
+});
 
-app.use('/', (req, res) => {
-    res.send('Hello');
+/////////////////////////////////////
+const mongoose = require('mongoose');
+
+    mongoose.connect('mongodb://localhost:27017/beers', { useNewUrlParser: true })
+mongoose.connection.on('connected', () => {
+   console.log('Connected to "beers" database');
+})
+mongoose.connection.on('error', (err) => {
+   console.log(`Got an error!:\n${err}`);
 })
 
-/////////////////////////////////////
 const port = process.env.PORT || 4444;
 
 app.listen(port);
